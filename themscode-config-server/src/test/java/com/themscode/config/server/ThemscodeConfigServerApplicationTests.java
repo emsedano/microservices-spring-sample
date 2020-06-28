7a605befa21ed95ecae57b573fcefb8451dd14f3
Elias Martinez <emsedano@live.com.mx>
2020-06-28 10:04:23 -0500

    feat: added spring cloudn config server

---
+++ themscode-config-server/src/test/java/com/themscode/config/server/ThemscodeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themscode.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemscodeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
