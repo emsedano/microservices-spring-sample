e774cc7fea569f2f537019f5d0a402972dc14a67
Elias Martinez <emsedano@live.com.mx>
2020-06-28 21:09:12 -0500

    feat: reading from spring config (git)

--- themscode-products-item/src/main/java/com/themscode/core/items/controller/CartItemController.java
+++ themscode-products-item/src/main/java/com/themscode/core/items/controller/CartItemController.java
@@ -1,9 +1,17 @@
 package com.themscode.core.items.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +24,17 @@ import com.themscode.core.items.service.ItemService;
 @RestController
 public class CartItemController {
 	
+	Logger log = LoggerFactory.getLogger(CartItemController.class);
+	
 	@Autowired
 	@Qualifier("feignItemService")
 	private ItemService itemService;
+	
+	@Value("${environment}")
+	private String environment;
+	
+	@Value("${spring.application.name}")
+	private String serviceName;
 	
 	@HystrixCommand(fallbackMethod = "fallbackCartItems")
 	@GetMapping("/items")
@@ -38,6 +54,17 @@ public class CartItemController {
 	@GetMapping("/items/{id}/quantity/{quantity}")
 	public CartItem getCartItem(@PathVariable("id") Long id, @PathVariable("quantity") Integer quantity ) {
 		return this.itemService.item(id, 1);
+	}
+	
+	@GetMapping("/config")
+	public ResponseEntity<Map<String, String>> config() {
+		Map<String, String> config = new HashMap<>();
+
+		log.info(String.format("Service name: %s    environment: %s", serviceName, environment));
+		config.put("environment", environment);
+		config.put("serviceName", serviceName);
+		
+		return  new ResponseEntity<Map<String, String>>(config, HttpStatus.OK);
 	}
 
 }
